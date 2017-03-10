783a712f208be16879cac5058e0887a2c96a2e23
EduardoCruzProductions <eduardo_acj2@hotmail.com>
2017-03-10 16:46:55 -0300

    inicio da implementacao da classe

---
+++ app/src/main/java/eduardocruz/mydocuments/util/NameOperator.java
@@ -0,0 +1,25 @@
+package eduardocruz.mydocuments.util;
+
+
+public class NameOperator {
+
+    private String nome;
+
+    public NameOperator(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAbreviado(){
+
+
+
+        return null;
+    }
+
+    public String getMaiusculoPadrinizado(){
+
+
+
+        return null;
+    }
+}
