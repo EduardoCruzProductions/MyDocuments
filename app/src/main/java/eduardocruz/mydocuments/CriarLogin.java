3b8e37912f88dc4818378b4d06064963c4b50ae2
EduardoCruzProductions <eduardo_acj2@hotmail.com>
2017-03-10 01:36:07 -0300

    implementacao parte programatica da tela de criar usuario

--- app/src/main/java/eduardocruz/mydocuments/CriarLogin.java
+++ app/src/main/java/eduardocruz/mydocuments/CriarLogin.java
@@ -8,6 +8,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import eduardocruz.mydocuments.entidades.Usuario;
 
 public class CriarLogin extends AppCompatActivity {
 
@@ -23,8 +26,11 @@ public class CriarLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criar_login);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.label_title_criar_login);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         nome = (EditText) findViewById(R.id.editText_CriarLogin_nome);
         login = (EditText) findViewById(R.id.editText_CriarLogin_login);
@@ -33,5 +39,61 @@ public class CriarLogin extends AppCompatActivity {
         textError = (TextView) findViewById(R.id.textView_CriarLogin_error);
 
     }
+
+    public void cadastrar(View v){
+
+        String nome = this.nome.getText().toString();
+        String login = this.login.getText().toString();
+        String senha = this.senha.getText().toString();
+        String confSenha = this.confSenha.getText().toString();
+
+        if(nome.isEmpty() || login.isEmpty()){
+
+            textError.setText(R.string.error_preencher_todos_campos);
+
+        }else{
+
+            if(senha.equals(confSenha)){
+
+                if(senha.isEmpty()){
+
+                    textError.setText(R.string.error_preencher_campo_senha);
+
+                }else{
+
+                    textError.setText("");
+
+                    try{
+
+                        Usuario u = new Usuario();
+                        u.setNome(nome);
+                        u.setLogin(login);
+                        u.setSenha(senha);
+                        u.setPersist(false);
+
+                        u.save();
+
+                        finish();
+
+                    }catch(Exception e){
+
+                        Toast.makeText(getApplicationContext(), R.string.error, Toast.LENGTH_LONG).show();
+                        System.out.println("Codigo de erro: "+e.getMessage());
+
+                    }
+
+                }
+
+            }else{
+
+                textError.setText(R.string.error_senha_invalida);
+
+            }
+
+        }
+
+    }
+
+
 
 }
--- app/src/main/java/eduardocruz/mydocuments/Login.java
+++ app/src/main/java/eduardocruz/mydocuments/Login.java
@@ -1,7 +1,9 @@
 package eduardocruz.mydocuments;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,4 +26,13 @@ public class Login extends AppCompatActivity {
         persist = (CheckBox) findViewById(R.id.checkBox_login_persist);
 
     }
+
+
+    public void abrirCriarUsuario(View v){
+
+        Intent i = new Intent(Login.this, CriarLogin.class);
+        startActivity(i);
+
+    }
+
 }
